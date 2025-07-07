80590258b002b8702d03425e5809b9b4d3f37563
Jeuno2 <Jeuno2@users.noreply.github.com>
2025-07-07 11:42:27 -0500

    Add index.html README.md script.js

--- script.js
+++ script.js
@@ -1,64 +1,68 @@
 // global declarations
 let log = console.log; //shortening console.log
-let humanScore = 0;
-let computerScore = 0;
-let computerChoice = "";
-let humanChoice = "";
-
 
 // function to determine computer's selection of rock, paper or scissors
 function getComputerChoice() {
     const rpsArray = ["rock", "paper", "scissors"];
-    let computerNum = 0;
 
     // generate random number
     let randomNum = Math.floor(Math.random() * 3);
-    computerChoice = rpsArray[randomNum];
-
-    log(`Computer has chosen ${computerChoice}`);
-
-    return computerChoice;
-}
+    return rpsArray[randomNum];
+} //end function getComputerChoice()
 
 // function to determine human's selection of rock, paper or scissors
 function getHumanChoice() {
-    humanChoice = prompt("Enter selection of Rock, Paper or Scissors");
-    return humanChoice;
-}
+    return prompt("Enter selection of Rock, Paper or Scissors");
+} //end function getHumanChoice()
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    log(humanChoice);
-    if (humanChoice === computerChoice) {
-        log("result is a draw!");
-    }
-    else if (humanChoice === "rock" && computerChoice === "scissors") {
-        log("Player wins!");
-        humanScore++;
-    }
-    else if (humanChoice === "rock" && computerChoice === "paper") {
-        log("Computer wins!");
-        computerScore++;
-    }
-    else if (humanChoice === "paper" && computerChoice === "scissors") {
-        log("Computer wins!");
-        computerScore++;
-    }
-    else if (humanChoice === "paper" && computerChoice === "rock") {
-        log("Player wins!");
-        humanScoreScore++;
-    }
-    else if (humanChoice === "scissors" && computerChoice === "rock") {
-        log("Computer wins!");
-        computerScore++;
-    }
-    else if (humanChoice === "scissors" && computerChoice === "paper") {
-        log("Player wins!");
-        humanScore++;
-    }
+function playGame() {
+    // declaration of score keeping variables
+    let humanScore = 0;
+    let computerScore = 0;
 
-}
+    for (let round = 1; round <= 5; round++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        log(`Computer has selected: ${computerChoice}`);
 
-computerChoice = getComputerChoice();
-humanChoice = getHumanChoice();
-playRound(humanChoice, computerChoice);
+        playRound(humanChoice, computerChoice); //calls playRound() for a round to be played
+
+        function playRound(humanChoice, computerChoice) {
+            humanChoice = humanChoice.toLowerCase();
+            log(humanChoice);
+            if (humanChoice === computerChoice) {
+                log("result is a draw!");
+            }
+            else if (humanChoice === "rock" && computerChoice === "scissors") {
+                log("Player wins!");
+                humanScore++;
+            }
+            else if (humanChoice === "rock" && computerChoice === "paper") {
+                log("Computer wins!");
+                computerScore++;
+            }
+            else if (humanChoice === "paper" && computerChoice === "scissors") {
+                log("Computer wins!");
+                computerScore++;
+            }
+            else if (humanChoice === "paper" && computerChoice === "rock") {
+                log("Player wins!");
+                humanScoreScore++;
+            }
+            else if (humanChoice === "scissors" && computerChoice === "rock") {
+                log("Computer wins!");
+                computerScore++;
+            }
+            else if (humanChoice === "scissors" && computerChoice === "paper") {
+                log("Player wins!");
+                humanScore++;
+            }
+        } //end function playRound()
+            log(`Player score: ${humanScore}`);
+            log(`Computer score: ${computerScore}`);
+            log(`New round starting!`);
+            log(`--------------------------------`);
+    } // end for loop
+} //end function playGame()
+
+playGame(); //function call to start game
